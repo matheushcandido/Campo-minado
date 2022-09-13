ff5dc5c87819168b60fcdf0c9988181ed4619bdf
Matheus Henrique Candido <89227481+matheushcandido@users.noreply.github.com>
2022-09-13 15:09:27 -0300

    Add files via upload

---
+++ Ivory/Ivory.TesteEstagio.CampoMinado/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Ivory.TesteEstagio.CampoMinado
+{
+    class Program
+    {
+
+		static void Main(string[] args)
+        {
+
+            var campoMinado = new CampoMinado();
+            Console.WriteLine("Início do jogo\n=========");
+            Console.WriteLine(campoMinado.Tabuleiro);
+
+            // Realize sua codificação a partir deste ponto, boa sorte!
+
+            // Forma que o usuário coloca as coordenadas:
+
+            /*            while (campoMinado.JogoStatus == 0)
+                        {
+                            Console.WriteLine("Digite a nova coordenada:");
+                            Console.WriteLine("Digite a linha primeiro, depois a coluna.");
+                            campoMinado.Abrir(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+                            Console.WriteLine(campoMinado.Tabuleiro);
+
+                            if (campoMinado.JogoStatus == 1)
+                            {
+                                Console.WriteLine("Parabéns, você ganhou!");
+                            } else if (campoMinado.JogoStatus == 2)
+                            {
+                                Console.WriteLine("Que pena, você perdeu.");
+                            }
+                        }
+            */
+
+            // Forma autônoma:
+
+            Random rnd = new Random();
+
+            while (campoMinado.JogoStatus == 0)
+            {
+
+                    Console.WriteLine("Nova coordenada:");
+                    campoMinado.Abrir(rnd.Next(1, 10), rnd.Next(1, 10));
+                    System.Threading.Thread.Sleep(2000);
+                    Console.WriteLine(campoMinado.Tabuleiro);
+
+                    if (campoMinado.JogoStatus == 1)
+                {
+                    Console.WriteLine("Parabéns, você ganhou!");
+                }
+                    else if (campoMinado.JogoStatus == 2)
+                {
+                    Console.WriteLine("Que pena, você perdeu.");
+                }
+
+
+            }
+
+        }
+    }
+}
